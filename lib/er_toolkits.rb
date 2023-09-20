2c712c1b9d2ba0e6bfc00c621e177166086c810d
Ernest Lee <oolzpishere@126.com>
2023-09-20 13:19:49 +0800

    add ParseJson

--- lib/er_toolkits.rb
+++ lib/er_toolkits.rb
@@ -3,6 +3,7 @@
 require_relative "er_toolkits/version"
 
 require_relative "er_toolkits/hash_to_ordered_md5"
+require_relative "er_toolkits/parse_json"
 
 module ErToolkits
   class Error < StandardError; end
